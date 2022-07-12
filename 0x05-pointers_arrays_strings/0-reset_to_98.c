3f787e4d28597d45bbed760d070e0e87e95c7296
DaanytD <danaytbiruk07@gmail.com>
2022-07-12 02:10:09 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - main
+ * Description: updates the value it points to to 98
+ * @n: - pinter to int
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
